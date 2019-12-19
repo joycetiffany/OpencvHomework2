4d235d5419abf14c530dc5339ce3f3f201785a29
chuyuting <a11025112@gmail.com>
2019-12-19 12:25:24 +0800

    ar test folder

---
+++ ar_test/calibration_data/read.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+print('------------intrinsics.npy')
+print(np.load('intrinsics.npy'))
+
+
+print('-------------distortion.npy')
+print(np.load('distortion.npy'))
+
+
+print('----------------new_intrinsics.npy')
+print(np.load('new_intrinsics.npy'))
+
+
+print('-------------------roi.npy')
+print(np.load('roi.npy'))
